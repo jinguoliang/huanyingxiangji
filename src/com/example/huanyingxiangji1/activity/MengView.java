fee7ec7b2fb1b3da54b1000d14282025fcb5ef75
jinux111 <jinguol999@163.com>
2015-10-23 16:36:55 +0800

    feature: 双击适配蒙的大小为屏幕大小

--- src/com/example/huanyingxiangji1/activity/MengView.java
+++ src/com/example/huanyingxiangji1/activity/MengView.java
@@ -9,181 +9,208 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
+import com.example.huanyingxiangji1.utils.LogHelper;
+
 public class MengView extends ImageView {
 
-	private static final String TAG = MengView.class.getName();
-	private static final float TOILERENCE = 5;
-	int mAlpha = 5;
+    private static final String TAG = MengView.class.getName();
+    private static final float TOILERENCE = 5;
+    private Matrix mOrignalMatrix;
 
-	float mInitWidth;
-	float mInitHeight;
-	private Bitmap mBitmap;
+    private int mAlpha = 5;
 
-	PointF Origin = new PointF(0, 0);
+    private float mInitWidth;
+    private float mInitHeight;
 
-	public MengView(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
-	}
+    private PointF Origin = new PointF(0, 0);
 
-	public MengView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    private long mFirstTouchTime;
+    private float tmpWidth;
+    private float tmpHeight;
 
-	@Override
-	public void setImageBitmap(Bitmap bm) {
-		super.setImageBitmap(bm);
-		mInitHeight = bm.getHeight();
-		mInitWidth = bm.getWidth();
-		mBitmap = bm;
-	}
+    private int mTouchCount = 1;
 
-	// the touch down pointer x, y,
-	// if two pointer, the distance between them, the center pointer between
-	// them
-	float ox, oy, oPointerDistence;
-	PointF oCenterPosition;
+    // the touch down pointer x, y,
+    // if two pointer, the distance between them, the center pointer between
+    // them
+    float ox, oy, oPointerDistence;
+    PointF oCenterPosition;
 
-	float ddx, ddy;
-	private float tmpWidth;
-	private float tmpHeight;
+    public MengView(Context context) {
+        super(context);
+        saveOrignalMatrix();
+    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		float x = event.getX();
-		float y = event.getY();
+    public MengView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        saveOrignalMatrix();
+    }
 
-		int touchCount = event.getPointerCount();
+    private void saveOrignalMatrix() {
+        Matrix matrix = getImageMatrix();
+        mOrignalMatrix = new Matrix(matrix);
+    }
 
-		switch (event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
+    @Override
+    public void setImageBitmap(Bitmap bm) {
+        super.setImageBitmap(bm);
+        mInitHeight = bm.getHeight();
+        mInitWidth = bm.getWidth();
+    }
 
-			ox = x; // the touch down position
-			oy = y;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX(0);
+        float y = event.getY(0);
 
-			return true;
-		case MotionEvent.ACTION_MOVE:
-			if (touchCount == 1) { // on pointer move
-				//processOneTouch(x - ox, y - oy);
-			} else if (touchCount == 2) { // two pointer move
+        int mPointerCount = event.getPointerCount();
 
-				float x1 = event.getX(1);
-				float y1 = event.getY(1);
-				float x2 = event.getX(0);
-				float y2 = event.getY(0);
-				Log.e(TAG, "x1 = " + x1);
-				Log.e(TAG, "y1 = " + y1);
-				Log.e(TAG, "x2 = " + x2);
-				Log.e(TAG, "y2 = " + y2);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                ox = x; // the touch down position
+                oy = y;
+                LogHelper.i(TAG, "mTouchCount = " + mTouchCount);
+                if (mTouchCount == 1) {
+                    LogHelper.i(TAG, "scale touch first");
+                    mFirstTouchTime = System.currentTimeMillis();
+                } else if (mTouchCount == 2) {
+                    LogHelper.i(TAG, "scale touch second");
+                    if (System.currentTimeMillis() - mFirstTouchTime < 300) {
+                        scaleImageFitScreen();
+                    }
+                    mTouchCount = 0;
+                }
 
-				processTwoTouch(x1, y1, x2, y2);
+                return true;
+            case MotionEvent.ACTION_MOVE:
 
-			}
+                if (Math.abs(x - ox) > TOILERENCE || Math.abs(y - oy) > TOILERENCE) {
+                    mTouchCount = 0;
+                }
 
-			return true;
-		case MotionEvent.ACTION_UP:
+                if (mPointerCount == 1) { // on pointer move
+                    //processOneTouch(x - ox, y - oy);
+                } else if (mPointerCount == 2) { // two pointer move
 
-			Log.e(TAG, "touch up");
-			oPointerDistence = 0;
+                    float x1 = event.getX(1);
+                    float y1 = event.getY(1);
+                    float x2 = event.getX(0);
+                    float y2 = event.getY(0);
+
+                    processTwoTouch(x1, y1, x2, y2);
+                }
+                return true;
+            case MotionEvent.ACTION_UP:
+                mTouchCount++;
+                Log.e(TAG, "touch up");
 //			mScale = mNowScale;
 //			mSHeight = mNowHeight;
 //			mSWidth = mNowWidth;
 //			Origin.x = mBitmapOffsetX;
 //			Origin.y = mBitmapOffsetY;
-			mBitmapOffsetX = mBitmapOffsetY =0;
+                mBitmapOffsetX = mBitmapOffsetY = 0;
 
-			return true;
-		default:
-			return true;
-		}
+                return true;
+            default:
+                return true;
+        }
 
-	}
-	
-	float mBitmapOffsetX=0;
-	float mBitmapOffsetY=0;
+    }
 
-	private void processTwoTouch(float x1, float y1, float x2, float y2) {
+    /**
+     * 调整图片大小适应屏幕宽度
+     */
+    private void scaleImageFitScreen() {
+        LogHelper.i(TAG, "scaleImageFitScreen");
+        setImageMatrix(mOrignalMatrix);
+        invalidate();
+    }
 
-		float pointerDistence = (float) Math.sqrt(Math.pow(x1 - x2, 2)
-				+ Math.pow(y1 - y2, 2));
-		PointF targetCenter = new PointF((x1 + x2) / 2, (y1 + y2) / 2);
-		Log.e(TAG, "center = " + targetCenter);
-		Log.e(TAG, "touch two");
-		Log.e(TAG, "pointerDistence =" + pointerDistence);
+    float mBitmapOffsetX = 0;
+    float mBitmapOffsetY = 0;
 
-		Log.e(TAG, "touch move");
-		if (oPointerDistence == 0) {
-			oPointerDistence = pointerDistence;
-			oCenterPosition = targetCenter;
-			return;
-		}
-		
-		if (Math.abs(oPointerDistence - pointerDistence) > TOILERENCE * 2
-				|| Math.abs(targetCenter.x - oCenterPosition.x) > TOILERENCE
-				|| Math.abs(targetCenter.y - oCenterPosition.y) > TOILERENCE) {
-			
-			Matrix matrix = getImageMatrix();
+    private void processTwoTouch(float x1, float y1, float x2, float y2) {
 
-			float scale = pointerDistence / oPointerDistence;
-			matrix.postScale(scale, scale);
-			
-			Log.e(TAG,"scale = "+scale);
-			mNowScale = mNowScale * scale;
+        float pointerDistence = (float) Math.sqrt(Math.pow(x1 - x2, 2)
+                + Math.pow(y1 - y2, 2));
+        PointF targetCenter = new PointF((x1 + x2) / 2, (y1 + y2) / 2);
+        Log.e(TAG, "center = " + targetCenter);
+        Log.e(TAG, "touch two");
+        Log.e(TAG, "pointerDistence =" + pointerDistence);
 
-			
-			
-			tmpWidth = (oCenterPosition.x - Origin.x);
-			tmpHeight = (oCenterPosition.y - Origin.y);
+        Log.e(TAG, "touch move");
+        if (oPointerDistence == 0) {
+            oPointerDistence = pointerDistence;
+            oCenterPosition = targetCenter;
+            return;
+        }
 
-			PointF center2 = new PointF(tmpWidth * scale, tmpHeight
-					* scale);
+        if (Math.abs(oPointerDistence - pointerDistence) > TOILERENCE * 2
+                || Math.abs(targetCenter.x - oCenterPosition.x) > TOILERENCE
+                || Math.abs(targetCenter.y - oCenterPosition.y) > TOILERENCE) {
 
-			float dx = targetCenter.x - center2.x;
-			float dy = targetCenter.y - center2.y;
+            //TODO copy matrix and set that matrix
+            Matrix matrix = getImageMatrix();
 
-			mBitmapOffsetX+=dx;
-			mBitmapOffsetX+=dy;
-			matrix.postTranslate(dx, dy);
+            float scale = pointerDistence / oPointerDistence;
+            matrix.postScale(scale, scale);
 
-			Log.e(TAG,"matrix = "+matrix);
-			oPointerDistence = pointerDistence;
-			oCenterPosition = targetCenter;
-			
-			invalidate();
-		}
+            Log.e(TAG, "scale = " + scale);
+            mNowScale = mNowScale * scale;
 
-	}
 
-	private void processOneTouch(float dx, float dy) {
+            tmpWidth = (oCenterPosition.x - Origin.x);
+            tmpHeight = (oCenterPosition.y - Origin.y);
 
-		if (dx > TOILERENCE) { // swipe to right
-			// TODO What's up?
-		} else if (dx < -TOILERENCE) {// swipe to left
-			// TODO what's up?
-		} else if (dy > TOILERENCE) {// swipe to down -- minus the
-										// aplha
-			if (mAlpha != 0) {
-				mAlpha -= 5;
-				this.setAlpha(mAlpha);
-			}
-			oy = oy + dy;
-		} else if (dy < -TOILERENCE) {// swipe to up -- plus the
-										// alpha
-			if (mAlpha != 240) {
-				mAlpha += 5;
-				this.setAlpha(mAlpha);
-			}
-			oy = oy + dy;
-		}
-	}
+            PointF center2 = new PointF(tmpWidth * scale, tmpHeight
+                    * scale);
 
-//	float mScale = 1;
-	float mNowScale = 1;
+            float dx = targetCenter.x - center2.x;
+            float dy = targetCenter.y - center2.y;
 
-	float mSWidth = 1;
-	float mSHeight = 1;
-	float mNowWidth = 1;
-	float mNowHeight = 1;
+            mBitmapOffsetX += dx;
+            mBitmapOffsetX += dy;
+            matrix.postTranslate(dx, dy);
+
+            Log.e(TAG, "matrix = " + matrix);
+            oPointerDistence = pointerDistence;
+            oCenterPosition = targetCenter;
+
+            invalidate();
+        }
+
+    }
+
+    private void processOneTouch(float dx, float dy) {
+
+        if (dx > TOILERENCE) { // swipe to right
+            // TODO What's up?
+        } else if (dx < -TOILERENCE) {// swipe to left
+            // TODO what's up?
+        } else if (dy > TOILERENCE) {// swipe to down -- minus the
+            // aplha
+            if (mAlpha != 0) {
+                mAlpha -= 5;
+                this.setAlpha(mAlpha);
+            }
+            oy = oy + dy;
+        } else if (dy < -TOILERENCE) {// swipe to up -- plus the
+            // alpha
+            if (mAlpha != 240) {
+                mAlpha += 5;
+                this.setAlpha(mAlpha);
+            }
+            oy = oy + dy;
+        }
+    }
+
+    //	float mScale = 1;
+    float mNowScale = 1;
+
+    float mSWidth = 1;
+    float mSHeight = 1;
+    float mNowWidth = 1;
+    float mNowHeight = 1;
 
 //	private void changeScale(float value) {
 //		Log.e(TAG, "the scale is " + value);
